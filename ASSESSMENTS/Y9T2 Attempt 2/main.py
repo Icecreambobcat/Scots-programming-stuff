dac810dc0945af409e6000973b17423f8b4e4203
Icecreambobcat <icekingrex123@gmail.com>
2023-06-24 00:58:22 +1000

    delete old attempts

--- ASSESSMENTS/Y9T2 Attempt 2/main.py
+++ ASSESSMENTS/Y9T2 Attempt 2/main.py
@@ -1,2 +1,23 @@
 import pygame
 from pygame.locals import *
+
+pygame.init()
+screen_width = 1000
+screen_height = 1000
+
+screen = pygame.display.set_mode((screen_width, screen_height))
+pygame.display.set_caption('Rex\'s Platformer game')
+
+bg_img = pygame.image.load('ASSESSMENTS/Y9T2 Attempt 2/Assets/BG.jpg')
+
+
+
+
+
+run = True
+while run: 
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            run = False
+
+pygame.quit()
